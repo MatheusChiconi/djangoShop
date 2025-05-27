b950afeae992c533cd822c3de91554f0a112bf10
matheus <matheusfchiconi@gmail.com>
2025-05-27 18:05:43 -0300

    Implement user login functionality with email and password; enhance login template with improved UI and error handling

--- contas/views.py
+++ contas/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login_user(request):
-    return render(request, 'contas/login.html', {})
+    email = ""
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        try:
+            user_obj = User.objects.get(email=email)
+            username = user_obj.username
+        except User.DoesNotExist:
+            messages.error(request, 'Usuário ou senha inválidos.')
+            return render(request, 'contas/login.html', {'email': email})
 
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos.')
+            return render(request, 'contas/login.html', {'email': email})
+    
+    context = {
+        'email': email,
+    }
+    return render(request, 'contas/login.html', context)
